1941c866e1a864a8bd9ffbde179e50778dadd88e
Parthipan Sekar <parthipan.sekar@loblaw.ca>
2022-05-24 14:15:51 -0400

    4th commit - 1

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ export default {
         return {
           render: () => {
             ReactDOM.render(<ChatWidget
-                // clientKey="1233444"
-                // headerTitle={init.headerTitle}
                 selector="#chat-app"
             />, document.querySelector('#chat-app'));
           },
