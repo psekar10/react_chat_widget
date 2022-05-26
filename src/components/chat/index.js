4dfbb00d4270c4bae0c15e7ca0341b6c1649fbd4
Parthipan Sekar <parthipan.sekar@loblaw.ca>
2022-05-25 22:06:15 -0400

    experimental change

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 // import { Widget } from 'react-chat-widget';
 import ChatBot from 'react-simple-chatbot';
+import Chat, { Message } from 'react-simple-chat';
+// Chat styles
+import 'react-simple-chat/src/components/index.css';
 // import './chat.css';
 
 const GitHubUrl = (props) =>{
@@ -60,77 +63,83 @@ const SmartChat = (props) =>{
   }
 
   return (
-    <ChatBot
-      floating={true}
-      opened={open}
-      headerTitle={headerTitle}
-      toggleFloating={toggleFloating}
-      botDelay={2000}
-      steps={[
-        {
-          id: '0',
-          message: `Hello There!`,
-          trigger: '1',
-        },
-        {
-          id: '1',
-          message: `I'm a widget that was embeded in this blog post!`,
-          trigger: '2',
-        },
-        {
-          id: '2',
-          message: `You can find my source code in this tutorial. Or.`,
-          trigger: '3',
-        },
-        {
-          id: '3',
-          component: <GitHubUrl />,
-          waitAction: true,
-          trigger: '4',
-        },
-        {
-          id: '4',
-          message: `I can even navigate you to a different sections on this page`,
-          trigger: 'sections',
-        },
-        {
-          id: 'sections',
-          options:[
-            { value: 'cdnv', label: 'CDN', trigger: 'cdn' },
-            { value: 'createv', label: 'Create React App', trigger: 'createapp' },
-          ]
-        },
-        {
-          id: 'cdn',
-          component: <CdnSection />,
-          waitAction: true,
-          trigger: 'again',
-        },
-        {
-          id: 'createapp',
-          component: <CreateAppSection />,
-          waitAction: true,
-          trigger: 'again',
-        },
-        {
-          id: 'again',
-          message: `Try Again?`,
-          trigger: 'new_try',
-        },
-        {
-          id: 'new_try',
-          options:[
-            { value: 'yes', label: 'YES', trigger: 'sections' },
-            { value: 'no', label: 'NO', trigger: 'end' },
-          ]
-        },
-        {
-          id: 'end',
-          end: true,
-          message: 'Bye Bye'
-        }
-      ]}
+    <Chat
+      title="Jane Doe"
+      user={{ id: 1 }}
+      messages={[]}
+      // onSend={message => setMessages([...messages, message])}
     />
+    // <ChatBot
+    //   floating={true}
+    //   opened={open}
+    //   headerTitle={headerTitle}
+    //   toggleFloating={toggleFloating}
+    //   botDelay={2000}
+    //   steps={[
+    //     {
+    //       id: '0',
+    //       message: `Hello There!`,
+    //       trigger: '1',
+    //     },
+    //     {
+    //       id: '1',
+    //       message: `I'm a widget that was embeded in this blog post!`,
+    //       trigger: '2',
+    //     },
+    //     {
+    //       id: '2',
+    //       message: `You can find my source code in this tutorial. Or.`,
+    //       trigger: '3',
+    //     },
+    //     {
+    //       id: '3',
+    //       component: <GitHubUrl />,
+    //       waitAction: true,
+    //       trigger: '4',
+    //     },
+    //     {
+    //       id: '4',
+    //       message: `I can even navigate you to a different sections on this page`,
+    //       trigger: 'sections',
+    //     },
+    //     {
+    //       id: 'sections',
+    //       options:[
+    //         { value: 'cdnv', label: 'CDN', trigger: 'cdn' },
+    //         { value: 'createv', label: 'Create React App', trigger: 'createapp' },
+    //       ]
+    //     },
+    //     {
+    //       id: 'cdn',
+    //       component: <CdnSection />,
+    //       waitAction: true,
+    //       trigger: 'again',
+    //     },
+    //     {
+    //       id: 'createapp',
+    //       component: <CreateAppSection />,
+    //       waitAction: true,
+    //       trigger: 'again',
+    //     },
+    //     {
+    //       id: 'again',
+    //       message: `Try Again?`,
+    //       trigger: 'new_try',
+    //     },
+    //     {
+    //       id: 'new_try',
+    //       options:[
+    //         { value: 'yes', label: 'YES', trigger: 'sections' },
+    //         { value: 'no', label: 'NO', trigger: 'end' },
+    //       ]
+    //     },
+    //     {
+    //       id: 'end',
+    //       end: true,
+    //       message: 'Bye Bye'
+    //     }
+    //   ]}
+    // />
   )
 }
 
